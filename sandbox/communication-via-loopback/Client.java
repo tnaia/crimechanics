76a3029376f7a91c95bf9e8949301eb61ffffec8
tassio.naia <tassio.naia@f8f514d3-1e69-a1ed-b813-3db2756e66e0>
2010-08-16 01:10:24 +0000

    Comunicação via loopback entre processos. Falta verificar se os dados recebidos são os que foram transmitidos.

---
+++ sandbox/communication-via-loopback/Client.java
@@ -0,0 +1,50 @@
+import java.net.*;
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public class Client {
+    public static void main(String[] args) {
+
+	System.out.println ("This is the client (Java).\n");
+	try {
+	    InetSocketAddress addr = new InetSocketAddress("localhost", 9999);
+	    Socket s = new Socket();
+	    // Differently from the C++ version, in this case we attempt to connect
+	    // with an infinite timeout
+	    s.connect(addr, 0);
+	    // Once connected, we try to receive one long int (32 bits)
+	    InputStream inp = s.getInputStream();
+	    byte[] data = new byte[4];
+	    ByteBuffer buffer = ByteBuffer.wrap(data);
+	    buffer.clear();
+	    int r = 0;
+	    do {
+		r += inp.read(data, r, 4-r);
+	    }
+	    while (r < 4);
+	    System.out.print("(Java): Received data: ");
+	    System.out.print(buffer.getInt());
+	    System.out.print(" = ");
+	    /*
+	      Isto
+	      System.out.println(Integer.toString(data, 16).toUpperCase());
+	      deveria imprimir os bytes, mas a compilação diz
+	      javac Client.java
+	      Client.java:28: cannot find symbol
+	      symbol  : method toString(byte[],int)
+	      location: class java.lang.Integer
+	      System.out.println(Integer.toString(data, 16).toUpperCase());
+	                                        ^
+	    */
+	    // Then return the same long int (32 bits)
+	    buffer.clear();
+	    buffer.putInt(42);
+	    OutputStream out = s.getOutputStream();
+	    out.write(data);
+	    s.close();
+	}
+	catch(Exception ex) {
+	    ex.printStackTrace();
+	}
+    }
+}
---
+++ sandbox/communication-via-loopback/server.cpp
@@ -0,0 +1,51 @@
+#include "SDL/SDL_net.h"
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+
+  std::cout << "This is the server (C++).\n";
+
+  if (SDLNet_Init() < 0)
+    {
+      std::cerr << "SDL_Init: " << SDL_GetError() << "\n";
+      exit (1);
+    }
+
+  std::cout << "Hello from SDLNet.\n";
+
+  IPaddress ip;
+  TCPsocket tcpsock, new_tcpsock;
+
+  if(SDLNet_ResolveHost(&ip,NULL,9999)==-1) {
+    printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+    exit(1);
+  }
+
+  tcpsock=SDLNet_TCP_Open(&ip);
+  if(!tcpsock) {
+    printf("SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+    exit(2);
+  }
+  printf("Waiting for connection...\n");
+  do {
+    new_tcpsock = SDLNet_TCP_Accept(tcpsock);
+    SDL_Delay(1);
+  }
+  while (!new_tcpsock);
+
+  // Once connected, I try to send some data                                                                                                             
+  long data = 114;
+  SDLNet_TCP_Send(new_tcpsock, &data, sizeof(long));
+  // Then I wait for a long int in response                                                                                                              
+  SDLNet_TCP_Recv(new_tcpsock, &data, sizeof(long));
+  // And I print the response:                                                                                                                           
+  printf("(C++): Got a response: %ld\n", data);
+
+  SDLNet_TCP_Close(tcpsock);
+  SDLNet_TCP_Close(new_tcpsock);
+
+
+  SDLNet_Quit ();
+  return 0;
+}
