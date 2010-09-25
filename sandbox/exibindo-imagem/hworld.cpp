74da1980d37466d1797ecf6fd2a1947acacb15a4
tassio.naia <tassio.naia@f8f514d3-1e69-a1ed-b813-3db2756e66e0>
2010-09-25 13:14:47 +0000

    Desenhando bmp na tela.

---
+++ sandbox/exibindo-imagem/hworld.cpp
@@ -0,0 +1,35 @@
+#include "SDL/SDL.h" // Talvez seja "SDL.h" no windows.
+
+
+int main( int argc, char **argv )
+{
+  //The images
+  SDL_Surface* hello = NULL;
+  SDL_Surface* screen = NULL;
+
+  //Start SDL
+  SDL_Init( SDL_INIT_EVERYTHING );
+
+  //Set up screen
+  screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+
+  //Load image
+  hello = SDL_LoadBMP( "hello.bmp" );
+
+  //Apply image to screen
+  SDL_BlitSurface( hello, NULL, screen, NULL );
+
+  //Update screen
+  SDL_Flip( screen );
+
+  //Pause
+  SDL_Delay( 2000 ); //2000 miliseconds
+
+
+  //Free the loaded image
+  SDL_FreeSurface( hello );
+
+  //Quit SDL
+  SDL_Quit();
+  return 0;
+}
