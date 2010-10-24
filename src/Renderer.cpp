b01e5b9e62d9e2a6cc48a31e9fb09518944de76d
tassio.naia <tassio.naia@f8f514d3-1e69-a1ed-b813-3db2756e66e0>
2010-10-24 16:29:09 +0000

    Caixa de diálogos! Agora restam só ajustes finos.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -19,7 +19,7 @@
  * which returns NULL in case.
 */
 
-std::vector<std::string> 
+std::vector<std::string> *
 shape_to_fit_rectangle( std::vector<std::string> & lines, 
 			TTF_Font * f, 
 			int w, 
@@ -33,16 +33,16 @@ shape_to_fit_rectangle( std::vector<std::string> & lines,
   // I'll make lines fit horizontally first
   int accumulated_height = 0;
   bool found_place_to_cut_vertically = false;
-  int place_to_cut_vertically = 0;
+  vector<string>::size_type place_to_cut_vertically = 0;
   // For each line
   for( vector<string>::size_type i = 0; i < lines.size(); ++i )
     {
-      std::cout<< "chopping line " << i << std::endl;
+      std::cout<< "chopping line " << i + 1 << " of " << lines.size()  << ": "<< std::endl << lines[ i ] << std::endl;
       string space = " ";
       /* I do a reverse search lines[ i ] for spaces (the only character 
        * a which I break lines.
        */
-      string::size_type pos = (lines[ i ]).size() - 1; 
+      string::size_type pos = (lines[ i ]).size(); 
       int line_w;
       int line_h;
       string reduced_line = lines[ i ].substr( 0, pos );
@@ -51,59 +51,66 @@ shape_to_fit_rectangle( std::vector<std::string> & lines,
 	  std::cerr << "! Couldn't calculate the size of text:" 
 		    << lines[ i ] << std::endl;
 	}
+      int initial_line_w = line_w;
+      std::cout << "line's initial width: "<< line_w << std::endl;
       while( line_w > w )
 	{
-	  string::size_type new_pos = lines[ i ].find_last_of( space, pos );
+	  string::size_type new_pos = lines[ i ].find_last_of( space, pos - 1 );
 	  std::cout << "posição "<< pos << std::endl;
-	  if( new_pos == pos )
+	  if( new_pos == string::npos )
 	    break;
 	  pos = new_pos;
 	  reduced_line = lines[ i ].substr( 0, pos );
+	  std::cout<< lines[ i ] << std::endl;
+	  std::cout<< reduced_line << std::endl;
 	  if( TTF_SizeUTF8( f, reduced_line.c_str(), & line_w, &line_h ) )
 	    {
 	      std::cerr << "! Couldn't calculate the size of text:" 
 			<< lines[ i ] << std::endl;
 	    }
 	}
-
+      std::cout << "line's final width: "<< line_w << " ("<< (initial_line_w - line_w) << " remains)" << std::endl;
       /* reduced line has the longest substring of lines[ i ] whose
        * rendering is smaller than w, or the longest substring of
        * lines[ i ] without spaces.
        */
-      
       if( reduced_line.size() < lines[ i ].size() && reduced_line.size() > 0 )
 	{
 	  //Let me cut this line in two for ya!
 	  string remainder_of_line = 
 	    lines[ i ].substr( reduced_line.size() );
-	  if( i < 
-	  lines.insert( lines.begin() + i + 1, remainder_of_line ); // tirei um +1
+	  if( i < lines.size() - 1 )
+	    {
+	      lines.insert( lines.begin() + i + 1, remainder_of_line ); // tirei um +1
+	    }
+	  else
+	    lines.push_back( remainder_of_line );
 	  lines[ i ] = reduced_line;
-	  }
+	}
       lines[ i ] = reduced_line;
 
-      accumulated_height += line_h;
+      accumulated_height += TTF_FontLineSkip( f ) + interline_space;
       if( !found_place_to_cut_vertically 
 	  && ( accumulated_height > h 
-	       || accumulated_height + interline_space > h ) )
+	       || accumulated_height + interline_space + TTF_FontLineSkip( f ) > h ) )
 	{
 	  found_place_to_cut_vertically = true;
 	  place_to_cut_vertically = i;
+	  std::cout << " -- cuttin here -- " << std::endl;
 	}
     }
-
   //Splits vector lines in two.
-  vector<string> lines_to_return;
+  vector<string> * lines_to_return = NULL;
   if( found_place_to_cut_vertically )
     {
-      copy( lines.begin() + place_to_cut_vertically, 
-	    lines.end(), 
-	    lines_to_return.begin() );
-
-      lines.erase( lines.begin() + place_to_cut_vertically,
-		   lines.end() );
+      lines_to_return = new vector<string> ();
+      while( lines.size() > place_to_cut_vertically )
+	{
+	  lines_to_return->push_back( lines[ place_to_cut_vertically ] );
+	  lines.erase( lines.begin() + place_to_cut_vertically );
+      std::cout<<"ouch"<<std::endl;
+	}
     }
-
     return lines_to_return;
 }
 
@@ -137,14 +144,20 @@ bool write_on_surface( SDL_Surface * s,
   bool ret = true; // Success or failuire of writing
 
   SDL_Surface * message = NULL;
+  int line_w = 0;
+  int line_h = 0;
+  int skip = TTF_FontLineSkip( f );
   for( vector<string>::size_type i = 0; i < lines.size(); ++i )
     {
-      message = TTF_RenderUTF8_Solid( f, lines[ i ].c_str(), c );
-      SDL_BlitSurface( message, NULL, s, & rect );
-      int line_h;
-      if( TTF_SizeUTF8( f, lines[ i ].c_str(), NULL, & line_h ) )
+      if( TTF_SizeUTF8( f, lines[ i ].c_str(), & line_w, & line_h ) )
 	ret = false;
-      rect.y += interline_space + line_h;
+      rect.w = line_w;
+      rect.h = line_h;
+
+      message = TTF_RenderUTF8_Blended( f, lines[ i ].c_str(), c );
+      SDL_BlitSurface( message, NULL, s, & rect );
+
+      rect.y += skip + interline_space;
     }
 
   return ret;
--- src/main-Renderer.cpp
+++ src/main-Renderer.cpp
@@ -27,15 +27,17 @@ int main( int argc, char ** argv )
   SDLManager sdlman = SDLManager::getInstance();
 
   vector<string> texto;
+  texto.push_back( string( "a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a." ) );
   texto.push_back( string( "Olê yayá!" ) );
   texto.push_back( string( "Um autor emblemático, que por sua atuação política, retrata bem a fusão etre os campos histórico e político, é o franês François G. Guisot, para quem as invações bárbaras teriam causado a decadência da civilização romana." ) );
   texto.push_back( string( "Cete manière de juger l'histoire d'Allemagne est conforme aux documents historiques des siècles passés." ) );
   texto.push_back( string( "Um louco, e com todo o direito de sê-lo!" ) );
 
-  vector<string> copia_texto;
-  copia_texto.resize( texto.size() );
+  vector<string> * copia_texto = NULL;
+  copia_texto = new vector<string> ();
+  copia_texto->resize( texto.size() );
 
-  copy( texto.begin(), texto.end(), copia_texto.begin() );
+  copy( texto.begin(), texto.end(), copia_texto->begin() );
 
   /*
   for( vector<string>::size_type i = 0; i < copia_texto.size(); ++i )
@@ -48,9 +50,12 @@ int main( int argc, char ** argv )
   SDL_FillRect( screen, 
 		&screen->clip_rect, 
 		SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+
   SDL_Flip( screen );
   TTF_Font * font = 
     TTF_OpenFont( "../media/fonts/Cash_Font_by_despicablehero.ttf", 20 );
+  //  TTF_Font * font = 
+  //  TTF_OpenFont( "../media/fonts/newathu.ttf", 15 );
   if( font == NULL )
     {
       std::cerr << "! Couldn't open font." << std::endl;
@@ -62,13 +67,35 @@ int main( int argc, char ** argv )
     texto[ 0 ].substr( 0, texto[ 0 ].find_last_of( string( " " ), texto[ 0 ].size() - 1 ) ) << "." << std::endl;
   return 0;
   */
-  std::cout << "o texto tem " << copia_texto.size() << " linhas" << std::endl;//return 0;
-  shape_to_fit_rectangle( copia_texto, font, 200, 200 );
-  SDL_Rect rect = { 10, 10, 200, 200 };
-  SDL_Color color = { 255, 0, 0 }; 
-  write_on_surface( screen, font, color, copia_texto, rect );
 
+  std::cout << "o texto tem " << copia_texto->size() << " linhas" << std::endl;//return 0;
+  vector<string> * next_texto = NULL;
+  SDL_Rect rect = { 50, 100, 250, 6.5 * TTF_FontLineSkip( font ) };
+  next_texto = shape_to_fit_rectangle( *copia_texto, font, rect.w, rect.h );
+  SDL_Color color = { 55, 80, 170 }; 
+  SDL_FillRect( screen, 
+		&rect, 
+		SDL_MapRGBA( screen->format, 0x10, 0xC0, 0xA0, 0xF0 ) );
+  write_on_surface( screen, font, color, *copia_texto, rect );
   SDL_Flip( screen );
-  SDL_Delay( 10000 );
+  SDL_Delay( 2000 );
+  while( next_texto != NULL )
+    {
+      //      SDL_Rect rect = { 10, 10, 250, 100 };
+      std::cout << "o texto tem " << next_texto->size() << " linhas" << std::endl;//return 0;
+      delete copia_texto;
+      copia_texto = next_texto;
+      next_texto = shape_to_fit_rectangle( *copia_texto, font, rect.w, rect.h );
+      std::cout << "o texto tem " << copia_texto->size() << " linhas" << std::endl;//return 0;
+      SDL_FillRect( screen, 
+		    &screen->clip_rect, 
+		    SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+      SDL_FillRect( screen, 
+		    &rect, 
+		    SDL_MapRGBA( screen->format, 0x10, 0xC0, 0xA0, 0xF0 ) );
+      write_on_surface( screen, font, color, *copia_texto, rect );
+      SDL_Flip( screen );
+      SDL_Delay( 2000 );
+    }
   return 0;
 }
