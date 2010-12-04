7b9801f96c9bb9769eb7a2f4a8c04a214b4310b2
tassio.naia <tassio.naia@f8f514d3-1e69-a1ed-b813-3db2756e66e0>
2010-12-04 15:35:26 +0000

    main

---
+++ trunk/src/main.cpp
@@ -0,0 +1,29 @@
+/* main.cpp
+ *
+
+ * Author: Tássio Naia dos Santos
+ * This is free software.
+ */
+
+#include<iostream>
+
+int main( int argc, char ** argv )
+{
+  pid_t pid = fork();
+  if( pid == 0 ) // child
+    execl( "jogo-cpp", 
+	   "./jogo-cpp", 
+	   ( char * ) NULL );
+  else if( pid < 0 )
+    std::cout << "Error: forking failed." << std::endl;
+  else
+    {
+      execl( "/bin/sh", 
+	     "sh", "-c", 
+	     "java -jar jogo-java.jar", 
+	     (char *) NULL );
+      std::cout << "Error: failed to run jogo-java.jar" 
+		<< std::endl;
+    }
+  return 0;
+}
