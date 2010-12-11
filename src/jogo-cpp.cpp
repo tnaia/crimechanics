5cd99c68d8e804f189811c0dcfb9e941472d5f80
tassio.naia <tassio.naia@f8f514d3-1e69-a1ed-b813-3db2756e66e0>
2010-12-11 21:19:05 +0000

    Pequenas alterações na comunicação.

--- src/jogo-cpp.cpp
+++ src/jogo-cpp.cpp
@@ -1,62 +1,62 @@
-/* jogo-cpp.cpp
- *
- * Author: Tássio Naia dos Santos
- * This is free software.
- */
+#include "SDL/SDL_net.h"
+#include <iostream>
 
-
-
-/* SDL libraries for rendering, playing sounds,
- *  capturing input, etc.
- */
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
-#include "SDL/SDL_ttf.h"
-#include "SDL/SDL_mixer.h"
-#include "SDL/SDL_gfx.h"
-
-/* Our libraries */
-#include "MyStateManager.hpp"
-#include "Renderer.hpp"
-
-// Concrete states...
-
-class Time {
- public:
-  static Time GetTime()  {  return SDL_GetTicks(); };
- Time( Uint32 t ) : time( t );
- private:
-  Uint32 time;
-};
-
-int main( int argc, char ** argv )
+int main (int argc, char **argv)
 {
 
-  // Start SDL
-  if( init() == false )
+  std::cout << "C++: This is the server (C++).\n";
+
+  if (SDLNet_Init() < 0)
     {
-      std::cerr << "Erro: falha ao iniciar SDL." << std::endl;
-      return 1;
+      std::cerr << "SDL_Init: " << SDL_GetError() << "\n";
+      exit (1);
     }
 
-  Time t1 - Time::GetTime();
-  Time dt = 0;
-  bool quit = false;
-  MyStateManager & stateM =
-    MyStateManager::Instance();
-  while( !quit )
+  IPaddress ip;
+  TCPsocket tcpsock, new_tcpsock;
+  Uint16 port = 9999;
+  if(SDLNet_ResolveHost(&ip,NULL,port)==-1) {
+    printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+    exit(1);
+  }
+
+  tcpsock=SDLNet_TCP_Open(&ip);
+  if(!tcpsock) {
+    printf("SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+    exit(2);
+  }
+  printf("Waiting for connection...\n");
+  do {
+    new_tcpsock = SDLNet_TCP_Accept(tcpsock);
+    SDL_Delay(1);
+  }
+  while (!new_tcpsock);
+
+  // Once connected, I try to send some data                                                                                                             
+  unsigned long data = 114;
+  std::cout << "(C++): sending "<< data;
+  printf (" (hexa: %lX).\n", data);
+  SDLNet_TCP_Send(new_tcpsock, &data, sizeof(unsigned long));
+  // Then I wait for a long int in response                                                                                                              
+  SDLNet_TCP_Recv(new_tcpsock, &data, sizeof(unsigned long));
+  // And I print the response:                                                                                                                           
+  /* Reflecting byte sequence */
+  unsigned long ndata = 0;
+  int aux = sizeof( unsigned long );
+  unsigned long mask = 0xFF;
+  while( aux > 0 )
     {
-
-
-      State * pState = stateM.GetCurrentState();
-      if( pState != 0 )
-	{
-	  pState->Updade( dt );
-	  pState->Draw( pRenderer );
-	}
-      Timer t2 = Time::GetTime();
-      dt = t2 - t1;
-      t1 = t2;
+      ndata |=  ( ( mask & ( data >> ( 8 * ( aux -1 ) ) ) ) << (8 * ( sizeof( unsigned long) -aux ) ) );
+      --aux;
     }
+  data = ndata;
+  printf("(C++): Got a response: %lu (hexa %lX)\n", data, data );
 
+
+  SDLNet_TCP_Close(tcpsock);
+  SDLNet_TCP_Close(new_tcpsock);
+
+
+  SDLNet_Quit ();
+  return 0;
 }
