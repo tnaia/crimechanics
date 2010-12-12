41ba53b6a37c8c4d8a1be6e612dc4eddd2f3e384
murilo.cerone@gmail.com <murilo.cerone@gmail.com@f8f514d3-1e69-a1ed-b813-3db2756e66e0>
2010-12-12 17:45:39 +0000

    teste ClienteJava

---
+++ src/ClienteJava/src/clientejava/Teste.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clientejava;
+
+/**
+ *
+ * @author Murilo
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        Main novo = new Main();
+        novo.runCliente();
+    }
+
+}
